1d84fd2c02c42146e7a320755adec468da823521
hawken <xionghekuan@gmail.com>
2019-07-16 17:49:28 +0800

    Add Remove Element

---
+++ Array/remove-element/027_remove_element_test.go
@@ -0,0 +1,24 @@
+package array
+
+import "testing"
+
+func Test_RemoveElement(t *testing.T) {
+	type args struct {
+		nums []int
+		val  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "a", args: args{[]int{3, 2, 2, 3}, 3}, want: 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := RemoveElement(tt.args.nums, tt.args.val); got != tt.want {
+				t.Errorf("RemoveElement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
